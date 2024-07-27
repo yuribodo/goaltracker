99783f6b46ffedb0c149f0d9e23440e4a0d2e16e
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-07-26 23:20:22 -0400

    comeback button

--- Front/src/app/goals/page.tsx
+++ Front/src/app/goals/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import GoalCard from '../(components)/(goalCard)'; // Certifique-se de ajustar o caminho do arquivo conforme necessário
 import { Goal } from '../(types)/types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 const GoalsList = () => {
   const [goalsState, setGoalsState] = useState<Goal[]>([]);
@@ -32,6 +34,15 @@ const GoalsList = () => {
 
   return (
     <div className="container mx-auto py-8 px-4">
+      {/* Botão de Voltar com ícone */}
+      <button 
+        onClick={() => window.history.back()} 
+        className="mb-6 px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors flex items-center"
+      >
+        <FontAwesomeIcon icon={faArrowLeft} className="mr-2" />
+        Voltar
+      </button>
+      
       <h1 className="text-4xl font-extrabold mb-6 text-center text-gray-800">Metas Concluídas</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {goalsState.length > 0 ? (
