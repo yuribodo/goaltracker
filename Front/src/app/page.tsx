f52c004a6ef46553a64c3f02f4acce2c951f247f
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-07-29 20:36:26 -0400

    login page

--- Front/src/app/page.tsx
+++ Front/src/app/page.tsx
@@ -1,17 +1,21 @@
+// pages/index.tsx
 import Link from "next/link";
 import Header from "./(components)/(header)";
 import Goals from "./(components)/(goals)";
+import ProtectedRoute from "./(components)/ProtectedRoute";
 
 export default function Home() {
-    return (
-        <div className="min-h-screen flex flex-col">
-            <Header />
-            <Goals />
-            <Link href="/goals">
-                <div className="fixed bottom-4 ml-2  h-fit bg-green-600 w-fit cursor-pointer px-4 py-2 rounded-xl shadow-lg">
-                    <p className="text-white">Ver Metas Anteriores</p>
-                </div>
-            </Link>
-        </div>
-    );
+  return (
+    <ProtectedRoute>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <Goals />
+        <Link href="/goals">
+          <div className="fixed bottom-4 ml-2 h-fit bg-green-600 w-fit cursor-pointer px-4 py-2 rounded-xl shadow-lg">
+            <p className="text-white">Ver Metas Anteriores</p>
+          </div>
+        </Link>
+      </div>
+    </ProtectedRoute>
+  );
 }
