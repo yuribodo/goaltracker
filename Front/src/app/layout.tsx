4719234de6444d9d63296b0829a131a2ec82d425
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-07-29 21:19:17 -0400

    decided to not use clerk anymore

--- Front/src/app/layout.tsx
+++ Front/src/app/layout.tsx
@@ -1,12 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,13 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          {children}
-        </body>
-      </html>
-    </ClerkProvider>
-    
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }
--- Front/src/app/page.tsx
+++ Front/src/app/page.tsx
@@ -1,21 +1,17 @@
-// pages/index.tsx
 import Link from "next/link";
 import Header from "./(components)/(header)";
 import Goals from "./(components)/(goals)";
-import ProtectedRoute from "./(components)/ProtectedRoute";
 
 export default function Home() {
-  return (
-    <ProtectedRoute>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <Goals />
-        <Link href="/goals">
-          <div className="fixed bottom-4 ml-2 h-fit bg-green-600 w-fit cursor-pointer px-4 py-2 rounded-xl shadow-lg">
-            <p className="text-white">Ver Metas Anteriores</p>
-          </div>
-        </Link>
-      </div>
-    </ProtectedRoute>
-  );
+    return (
+        <div className="min-h-screen flex flex-col">
+            <Header />
+            <Goals />
+            <Link href="/goals">
+                <div className="fixed bottom-4 ml-2  h-fit bg-green-600 w-fit cursor-pointer px-4 py-2 rounded-xl shadow-lg">
+                    <p className="text-white">Ver Metas Anteriores</p>
+                </div>
+            </Link>
+        </div>
+    );
 }
