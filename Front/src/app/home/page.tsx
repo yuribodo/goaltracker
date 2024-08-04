be181805f822a9a81effca7540ef9751a5266519
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-08-03 22:08:03 -0400

    changing routes

---
+++ Front/src/app/home/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Header from "../_components/header";
+import Goals from "../_components/goals";
+import AuthWrapper from "../_components/AuthWrapper";
+
+export default function Home() {
+    return (
+        <AuthWrapper>
+            <div className="min-h-screen flex flex-col">
+                <Header />
+                <Goals />
+                <Link href="/goals">
+                    <div className="fixed bottom-4 ml-2  h-fit bg-green-600 w-fit cursor-pointer px-4 py-2 rounded-xl shadow-lg">
+                        <p className="text-white">Ver Metas Anteriores</p>
+                    </div>
+                </Link>
+            </div>
+        </AuthWrapper>
+        
+    );
+}
