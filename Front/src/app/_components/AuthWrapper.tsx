73c8a5741691fe565fc37529b535301faa55ba06
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-08-04 16:57:57 -0400

    log out

--- Front/src/app/_components/AuthWrapper.tsx
+++ Front/src/app/_components/AuthWrapper.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useEffect, useState, ReactNode } from 'react';
+import { useRouter } from 'next/navigation';
 
-const AuthWrapper = ({ children }: { children: React.ReactNode }) => {
+const AuthWrapper = ({ children }: { children: ReactNode }) => {
   const [loading, setLoading] = useState<boolean>(true);
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const router = useRouter();
@@ -28,6 +28,12 @@ const AuthWrapper = ({ children }: { children: React.ReactNode }) => {
     }
   }, [loading, isAuthenticated, router]);
 
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsAuthenticated(false);
+    router.replace("/login");
+  };
+
   if (loading) {
     return <div>Loading...</div>; // Você pode substituir isso por um spinner ou outro componente de carregamento
   }
@@ -36,7 +42,17 @@ const AuthWrapper = ({ children }: { children: React.ReactNode }) => {
     return null; // Não renderiza nada se não estiver autenticado
   }
 
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      <button
+        onClick={handleLogout}
+        className="fixed top-4 right-4 px-4 py-2 bg-red-600 text-white rounded-lg shadow-md hover:bg-red-700 transition duration-300"
+      >
+        Logout
+      </button>
+    </>
+  );
 };
 
 export default AuthWrapper;
